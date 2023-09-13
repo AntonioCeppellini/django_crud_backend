e67c4fd03fe28ae1f17c349fdfc3602b51205c20
AntonioCeppellini <antonio.ceppellini@gmail.com>
2023-09-13 21:10:27 +0200

    first commit

---
+++ APIproject/api/urls.py
@@ -0,0 +1,7 @@
+from .views import article_list, article_details
+from django.urls import path
+
+urlpatterns = [
+    path('articles', article_list),
+    path('articles/<int:pk>/', article_details)
+]

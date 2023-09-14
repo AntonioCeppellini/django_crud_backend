55ed5f13d14faba637c2fda9b443819f6baf5c75
AntonioCeppellini <antonio.ceppellini@gmail.com>
2023-09-14 11:07:03 +0200

    views and routers

--- APIproject/api/urls.py
+++ APIproject/api/urls.py
@@ -1,10 +1,18 @@
-from .views import ArticleList, ArticleDetails
-from django.urls import path
+from .views import ArticleViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+#####setting up our router#####
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename = 'articles')
+
 #####using class based api views is better because it will be more browsable and the code will be more DRY#####
 urlpatterns = [
+    path('', include(router.urls)),
+
     #####using class based api views#####
-    path('articles/', ArticleList.as_view()),
-    path('articles/<int:id>/', ArticleDetails.as_view() )
+    #path('articles/', ArticleList.as_view()),
+    #path('articles/<int:id>/', ArticleDetails.as_view() )
     
     #####using functions based api views#####
     #path('articles', article_list),
--- APIproject/api/views.py
+++ APIproject/api/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from .models import Article
 from .serializers import ArticleSerializer
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view, APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics, mixins, viewsets
+
 
 # Create your views here.
 #####this is function base api views#####
@@ -47,46 +48,81 @@ def article_details(request, pk):
         article.delete()
         return Response(status = status.HTTP_204_NO_CONTENT) """
 
-#####this is class based api views#####
+#####this is class based api views, using it with mixins help us to write more DRY code:D#####
 #####seeing all the articles#####
-class ArticleList(APIView):
+"""
+class ArticleList(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin):
+    #####defining the queryset#####
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
     #####get request to see all the articles#####
-    def get(self, resquest):
-        articles =Article.objects.all()
-        serializer = ArticleSerializer(articles, many = True)
-        return Response(serializer.data)
+    def get(self, request):
+        return self.list(request)
     #####post request to create a post#####
     def post(self, request):
+        return self.create(request)
+
+#####seeing a single article#####
+class ArticleDetails(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    #####defining the queryset#####
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+    #####the lookup_field help us to locate the article we want to visualize#####
+    lookup_field = 'id'
+    #####get request to see a single article by its id#####   
+    def get(self, request, id):
+        return self.retrieve(request, id = id)
+    #####put request to update a single article by its id#####
+    def put(self, request, id):
+        return self.update(request, id = id)
+    #####delete request to delete a single article by its id#####
+    def delete(self, request, id):
+        return self.destroy(request, id = id) """
+
+#####making views using viewsets#####
+""" class ArticleViewSet(viewsets.ViewSet):
+    #####getting all the articles#####
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many = True)
+        return Response(serializer.data)
+    #####creating an article using viewsets#####
+    def create(self, request):
         serializer = ArticleSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status = status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
-
-#####seeing a single article#####
-class ArticleDetails(APIView):
-    #####get a single article by its id#####
-    def get_object(self, id):
-        try:
-            return Article.objects.get(id = id)
-    
-        except Article.DoesNotExist:
-            return Response(status = status.HTTP_404_NOT_FOUND)
-    #####get request to see a single article by its id#####   
-    def get(self, request, id):
-        article = self.get_object(id = id)
+    #####retrieving a specific article#####
+    def retrieve(self, request, pk = None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk = pk)
         serializer = ArticleSerializer(article)
         return Response(serializer.data)
-    #####put request to update a single article by its id#####
-    def put(self, request, id):
-        article = self.get_object(id = id)
+    #####updating a specific article#####
+    def update(self, request, pk = None):
+        article = Article.objects.get(pk = pk)
         serializer = ArticleSerializer(article, data = request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status = status.HTTP_404_NOT_FOUND404)
-    #####delete request to delete a single article by its id#####
-    def delete(self, request, id):
-        article = self.get_object(id = id)
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+    #####deleting a specific article#####
+    def destroy(self, request, pk = None):
+        article = Article.objects.get(pk = pk)
         article.delete()
-        return Response(status = status.HTTP_204_NO_CONTENT) 
+        return Response(status = status.HTTP_204_NO_CONTENT) """
+        
+#####using generic viewsets with mixin(superfast way)#####
+""" class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer """
+
+#####using model viewsets(fastest way! :D)#####
+class ArticleViewSet(viewsets.ModelViewSet):
+    #####setting the queryset#####
+    #####a queryset is a collection of objects from the database#####
+    queryset = Article.objects.all()
+    #####serializer class used for validating input and serialize output#####
+    #####(serializers are responsible for converting objects into data types understandable by JavaScript and front-end frameworks)#####
+    serializer_class = ArticleSerializer
